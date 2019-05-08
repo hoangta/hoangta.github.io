debad311ef70eecc7abca9878c17e8302da5cd7d
Hoang Ta <vinhhoang.ta@2359media.com>
2019-05-09 01:27:14 +0700

    Updates

---
+++ precache-manifest.f6d8eba9f533f223f295a9c1ff897d9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383ce790f1f55e4ca34caa5b072be095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e8b55c621f09b5015aa",
+    "url": "/static/css/main.481daa49.chunk.css"
+  },
+  {
+    "revision": "ac0ffdc894722be960c6",
+    "url": "/static/js/2.380c7fba.chunk.js"
+  },
+  {
+    "revision": "6e8b55c621f09b5015aa",
+    "url": "/static/js/main.fbe4811e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
