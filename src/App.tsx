6bd0093ac5e11d6ee38e50f2b2cbbe927322fb1f
Hoang Ta <blue.hoangta@gmail.com>
2025-05-20 14:25:25 +0700

    rename folder components to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import PrivacyPolicy from './components/PrivacyPolicy'
+import Home from './pages/Home'
+import PrivacyPolicy from './pages/PrivacyPolicy'
 
 import './App.css'
 
